2042ca0f7b2bda1ba50633ae393e8edc5458ffa4
rusla23 <bogomolnikovr99@gmail.com>
2023-10-26 20:24:53 +0300

    homepage upcoming todos

--- todo/todoapp/urls.py
+++ todo/todoapp/urls.py
@@ -5,10 +5,9 @@ app_name = 'todo'
 
 urlpatterns = [
     path('', views.home, name='home'), 
+    path('home/', views.home, name='home'), 
     path('list/', views.todo_list, name='todo_list'),
     path('create/', views.todo_create, name='create_todo'),
-    path('<int:todo_id>/', views.todo_detail, name='todo_detail'),
-    path('todo_detail/', views.todo_detail, name='todo_detail_without_pk'),
     path('<int:pk>/update/', views.todo_update, name='update_todo'),
     path('<int:pk>/delete/', views.todo_delete, name='delete_todo'),
     path('<int:todo_id>/edit/', views.todo_edit, name='todo_edit'),
--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -3,17 +3,26 @@ from .models import Todo
 from .forms import TodoForm, TodoEditForm
 from django.http import Http404
 from django.contrib.auth.decorators import login_required
+from datetime import datetime, timedelta
 
 def home(request):
-    return render(request, 'todo/home.html')
+    # Calculate the start and end dates for this week
+    today = datetime.now().date()
+    next_week = today + timedelta(days=7)
+
+    todos_this_week = Todo.objects.filter(due_date__gte=today, due_date__lt=next_week)
+
+    return render(request, 'todo/home.html', {
+        'todos_this_week': todos_this_week,
+    })
 
 def todo_list(request):
     todos = Todo.objects.all()
     return render(request, 'todo/todo_list.html', {'todos': todos})
 
-def todo_detail(request, todo_id):
-    todo = get_object_or_404(Todo, pk=todo_id)
-    return render(request, 'todo/todo_detail.html', {'todo': todo})
+# def todo_detail(request, todo_id):
+#     todo = get_object_or_404(Todo, pk=todo_id)
+#     return render(request, 'todo/todo_detail.html', {'todo': todo})
 
 def todo_create(request):
     if request.method == 'POST':
@@ -44,7 +53,7 @@ def todo_edit(request, todo_id):
         if edit_form.is_valid():
             edit_form.save()
             # Redirect to the to-do detail page after editing
-            return redirect('todo:todo_detail', todo_id=todo_id)
+            return redirect('todo:todo_list')
     else:
         edit_form = TodoEditForm(instance=todo)
 
