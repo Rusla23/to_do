252a05c426d2eed10852edd15c64fff51ed47aa2
rusla23 <bogomolnikovr99@gmail.com>
2023-10-26 11:40:12 +0300

    css style

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('todoapp.urls')), 
+    path('', include('todoapp.urls')), 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
